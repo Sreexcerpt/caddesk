e47d4d0afe748b10c8d419544838fdec999a822f
Sreexcerpt <sree.excerpt@gmail.com>
2025-02-27 12:00:30 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,41 @@
-import React from 'react'
-
-// Routes//
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// component imports//
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route,useLocation } from "react-router-dom";
 import SideNavaBar from './Component/SideNavaBar/SideNavaBar'
 import Footer from './Component/Footer/Footer'
-import Header from './Component/Header/Header'
-
-
-// Pages import here//
 import DashBoard from './Pages/DashBoard/DashBoard'
 import LeadTracking from './Pages/LeadTracking/LeadTracking';
 import AdmissionQuery from './Pages/Admin/Admission Query/Admissionquery'
 import LeadScoring from './Pages/LeadScoring/LeadScoring';
 import StudentProfileManagement from './Pages/StudentLifecycleManagement/StudentProfileManagement/StudentProfileManagement';
 import CourseBatchEnrollment from './Pages/StudentLifecycleManagement/CourseBatchEnrollment/CourseBatchEnrollment';
+import Login from './Pages/login/login';
+const Layout = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+
+  return (
+    <div>
+      {!isLoginPage && <SideNavaBar />}
+      <Routes>
+        <Route path="/" element={<DashBoard />} />
+        <Route path='/admissionquery' element={<AdmissionQuery />} />
+        <Route path="/LeadTracking" element={<LeadTracking />} />
+        <Route path="/LeadScoring" element={<LeadScoring />} />
+        <Route path="/StudentProfileManagement" element={<StudentProfileManagement />} />
+        <Route path="/CourseBatchEnrollment" element={<CourseBatchEnrollment />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {!isLoginPage && <Footer />}
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-      <Router>
-      <SideNavaBar />
-        <Routes>
-          <Route path="/" element={<DashBoard />} />
-          <Route path='/admissionquery' element={<AdmissionQuery/>}/>
-          <Route path="/LeadTracking" element={<LeadTracking/>}/>
-          <Route path="/LeadScoring" element={<LeadScoring/>}/>
-          <Route path="/StudentProfileManagement" element={<StudentProfileManagement/>}/>
-          <Route path="/CourseBatchEnrollment" element={<CourseBatchEnrollment/>}/>
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  )
-}
+    <Router>
+      <Layout /> {/* Now inside Router, so useLocation works */}
+    </Router>
+  );
+};
 
-export default App
+export default App;
